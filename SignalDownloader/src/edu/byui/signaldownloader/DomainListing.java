307b792ced8ec18e9b59856dc8745f8bdce27d5e
Clay Diffrient <clay.diffrient@gmail.com>
2013-01-17 21:08:03 -0700

    Move to NetBeans project for simplified builds

---
+++ SignalDownloader/src/edu/byui/signaldownloader/DomainListing.java
@@ -0,0 +1,72 @@
+package edu.byui.signaldownloader;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * DomainListing
+ * @author Clay Diffrient
+ */
+
+public class DomainListing
+{
+   /**
+    * Holds listing of domains
+    */
+   ObservableList<String> mListing;
+
+   /**
+    * Holds instance.
+    */
+   private static DomainListing cInstance = null;
+
+   /**
+    * Constructor
+    */
+   protected DomainListing()
+   {
+      mListing = FXCollections.observableArrayList();
+   }
+
+   /**
+    * getInstance
+    */
+   public static DomainListing getInstance()
+   {
+      if (cInstance == null)
+      {
+         cInstance = new DomainListing();
+      }
+      return cInstance;
+   }
+
+   /**
+    * Gets the list
+    */
+   public ObservableList<String> getList()
+   {
+      return mListing;
+   }
+
+   /**
+    * Add to list
+    */
+   protected void addAll(String ... elements)
+   {
+      for (int i = 0; i < elements.length; i++)
+      {
+         mListing.addAll(elements[i]);
+      }
+   }
+
+   protected void remove(String ... elements)
+   {
+      for (int i = 0; i < elements.length; i++)
+      {
+         mListing.removeAll(elements[i]);
+      }
+   }
+
+
+
+}
